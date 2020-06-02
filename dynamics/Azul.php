23ad21ee67985d385a5678b91059c4bb1fa9928e
AnaPau1603 <paula_rosales@comunidad.unam.mx>
2020-06-01 20:14:47 -0500

    FEAT: Naranja y Rojo se casan malevolamente >:)

--- dynamics/Azul.php
+++ dynamics/Azul.php
@@ -2,7 +2,7 @@
 
 echo "<link rel='stylesheet' href='../statics/Verde.css'>";
 echo "<p id='verde'>Holaaa soy azul :D</p>";
-echo "<p id='naranja'>Yo soy hija de verde</p>";
-echo "<p id='rojo'>Holaaa soy azul :D</p>";
+echo "<p id='naranja'>Yo soy hija de verde, y ahora esposa de Rojo >:p</p>";
+echo "<p id='rojo'>Yo también mentí, soy Rojo muahaha >;)</p>";
 
 ?>
